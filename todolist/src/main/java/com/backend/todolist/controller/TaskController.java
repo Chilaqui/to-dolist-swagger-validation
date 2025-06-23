0cf751dfcc62182f09a8f6b596caf12f03c4e22e
Hector <comhector_@hotmail.com>
2025-06-23 16:42:27 -0600

    Configurando el Controller con los metodos creados

--- todolist/src/main/java/com/backend/todolist/controller/TaskController.java
+++ todolist/src/main/java/com/backend/todolist/controller/TaskController.java
@@ -1,9 +1,13 @@
 package com.backend.todolist.controller;
 
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.todolist.model.Task;
@@ -23,14 +27,36 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    // Crear una tarea
     @PostMapping
     public ResponseEntity<Task> createTask(@Valid @RequestBody TaskDTO taskDTO) {
         Task createdTask = taskService.createTask(taskDTO);
         return ResponseEntity.ok(createdTask);
     }
 
-    // Obtener tarea id
+    // Actualizar tarea por id
+    public ResponseEntity<Task>  updateTask(@PathVariable Long id,@Valid @RequestBody Task task){
+        Task updateTask = taskService.updateTask(id, null);
+        return ResponseEntity.ok(updateTask);
+    }
+    
     // Obtener Todas las Tareas
+
+    public ResponseEntity<List<Task>> getAllTask(){
+        List<Task> tasks = taskService.getAllTasks();
+        return ResponseEntity.ok(tasks);
+    }
+
+    //Obtener tarea por id
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id){
+        Task getTaskById = taskService.getTaskById(id);
+        return ResponseEntity.ok(getTaskById);
+    }
+
     // Eliminar Tarea
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- todolist/src/main/java/com/backend/todolist/service/TaskService.java
+++ todolist/src/main/java/com/backend/todolist/service/TaskService.java
@@ -7,13 +7,18 @@ import com.backend.todolist.model.Dtos.TaskDTO;
 
 public interface TaskService {
 
-    List<Task> getAllTasks();
-
-    Task getTaskById(Long id);
-
+     //Crear Tarea
     Task createTask(TaskDTO taskDTO);
 
+    //Modifiacar tarea por id
     Task updateTask(Long id, TaskDTO taskDTO);
 
+    //Obtener todas las tareas
+    List<Task> getAllTasks();
+
+    //Obtener tarea por id
+    Task getTaskById(Long id);
+
+    //Eliminar tarea
     void deleteTask(Long id);
 }
