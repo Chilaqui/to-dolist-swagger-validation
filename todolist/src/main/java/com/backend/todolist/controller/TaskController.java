24f57ace4536087f11538597f63af05ba69c909d
Hector <comhector_@hotmail.com>
2025-06-23 16:55:58 -0600

    Mapeando EndPoints

--- todolist/src/main/java/com/backend/todolist/controller/TaskController.java
+++ todolist/src/main/java/com/backend/todolist/controller/TaskController.java
@@ -4,10 +4,12 @@ package com.backend.todolist.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.todolist.model.Task;
@@ -28,32 +30,36 @@ public class TaskController {
     }
 
     // Crear una tarea
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Task> createTask(@Valid @RequestBody TaskDTO taskDTO) {
         Task createdTask = taskService.createTask(taskDTO);
         return ResponseEntity.ok(createdTask);
     }
 
     // Actualizar tarea por id
-    public ResponseEntity<Task>  updateTask(@PathVariable Long id,@Valid @RequestBody Task task){
-        Task updateTask = taskService.updateTask(id, null);
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Task>  updateTask(@PathVariable Long id,@Valid @RequestBody TaskDTO taskDTO){
+        Task updateTask = taskService.updateTask(id, taskDTO);
         return ResponseEntity.ok(updateTask);
     }
     
     // Obtener Todas las Tareas
 
+    @GetMapping
     public ResponseEntity<List<Task>> getAllTask(){
         List<Task> tasks = taskService.getAllTasks();
         return ResponseEntity.ok(tasks);
     }
 
     //Obtener tarea por id
+    @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id){
         Task getTaskById = taskService.getTaskById(id);
         return ResponseEntity.ok(getTaskById);
     }
 
     // Eliminar Tarea
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id){
         taskService.deleteTask(id);
         return ResponseEntity.noContent().build();
