ab76a3e42ac74eba575899dbf120d1836078a53c
Hector <comhector_@hotmail.com>
2025-06-23 17:43:51 -0600

    Agreagando anotaciones swagger en el TaskDTO

--- todolist/src/main/java/com/backend/todolist/model/Dtos/TaskDTO.java
+++ todolist/src/main/java/com/backend/todolist/model/Dtos/TaskDTO.java
@@ -7,19 +7,28 @@ import com.backend.todolist.emuns.TaskStatus;
 
 import jakarta.validation.constraints.NotNull;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "DTO para crear o actualizar tareas")
 public class TaskDTO {
+
+    @Schema(description = "Título de la tarea", example = "Estudiar Swagger")
     @NotNull(message = "El título es obligatorio")
     private String title;
 
+    @Schema(description = "Descripción detallada de la tarea", example = "Revisar las anotaciones de OpenAPI en Spring Boot")
     private String description;
 
+    @Schema(description = "Nivel de prioridad de la tarea", example = "HIGH")
     private Priority priority;
 
+    @Schema(description = "Estado actual de la tarea", example = "PENDING")
     private TaskStatus status;
 
+    @Schema(description = "Fecha de la última actualización de la tarea (opcional)", example = "2024-06-22T14:30:00")
     private LocalDateTime updatedAt;
 
-    
+    // Getters y Setters
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
     }
