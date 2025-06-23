6e9085c2a122f97d4124faf1963859f3f4afdec1
Hector <comhector_@hotmail.com>
2025-06-23 05:00:49 -0600

    Completamos la logica

--- todolist/src/main/java/com/backend/todolist/service/TaskService.java
+++ todolist/src/main/java/com/backend/todolist/service/TaskService.java
@@ -1,5 +1,18 @@
 package com.backend.todolist.service;
 
-public class TaskService {
+import java.util.List;
 
+import com.backend.todolist.model.Task;
+
+public interface TaskService {
+
+    List<Task> getAllTasks();
+
+    Task getTaskById(Long id);
+
+    Task createTask(Task task);
+
+    Task updateTask(Long id, Task taskDetails);
+
+    void deleteTask(Long id);
 }
--- todolist/src/main/java/com/backend/todolist/service/TaskServiceImpl.java
+++ todolist/src/main/java/com/backend/todolist/service/TaskServiceImpl.java
@@ -1,5 +1,61 @@
 package com.backend.todolist.service;
 
-public interface TaskServiceImpl {
-    
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.backend.todolist.model.Task;
+import com.backend.todolist.repository.TaskRepository;
+
+@Service
+public class TaskServiceImpl implements TaskService{
+
+    private final TaskRepository taskRepository;
+
+    public TaskServiceImpl (TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    //Obtener todas las tareas
+    @Override
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    //Obtener tareas por id
+    @Override
+    public Task getTaskById(Long id){
+        return taskRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Task not found with id"+ id));
+    }
+
+    //Crear Tareas
+    @Override 
+    public Task createTask(Task task){
+        task.setCreatedAt(LocalDateTime.now());
+        task.setUpdatedAt(LocalDateTime.now());
+        return taskRepository.save(task);
+    }
+
+    //Modificar una tarea
+    @Override
+    public Task updateTask(Long id, Task taskDetails){
+        Task task = getTaskById(id);
+
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setPriority(taskDetails.getPriority());
+        task.setUpdatedAt(taskDetails.getUpdatedAt());
+
+        return taskRepository.save(task);
+    }
+
+
+    public void deleteTask(Long id){
+        Task task = getTaskById(id);
+        taskRepository.delete(task);
+    }
+
 }
